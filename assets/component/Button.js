8f4c8efb4e3852b8360932ff47e9eb02351d2ad6
TayisiyaChernenko <tcx190026@utdallas.edu>
2023-02-12 04:27:59 -0600

    Save image functionality

--- assets/component/Button.js
+++ assets/component/Button.js
@@ -26,3 +26,4 @@ const styles = StyleSheet.create({
         marginLeft: 10
     }
 })
+
--- screens/CameraFeature.js
+++ screens/CameraFeature.js
@@ -1,24 +1,59 @@
-import { CurrentRenderContext } from "@react-navigation/native";
+import { CurrentRenderContext} from "@react-navigation/native";
 import { Camera } from "expo-camera";
 import { View } from "react-native";
+import { StyleSheet,Text,View, Image } from "react-native";
 import Button from "../assets/component/Button";
+import  {useRef} from 'react';
+
 
 export default function CameraFeature({ navigation }) {
     {}
     const cameraRef = useRef(null);
+    
+    if (hasCameraPermission ===false){
+        return <Text> No access to Camera </Text>
+    }
     return (
        <View style = {styles.container}>
+        {!image ? 
             <Camera
                 styles = {styles.camera}
                 type = {type}
                 flashMode = {flash}
                 ref = {cameraRef}
-            />
+            >  
+            </Camera>
+            :
+            <Image source = {{url: image}} style = {styles.camera}/>
+        }
             <View>
-                <Button title = {'Take a picture'} icon = "camera"/>
+                {image ? 
+                <View style = {{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingHorizontal: 50
+                }}>
+                    <Button title = {'Save'} icon = "check" />  
+                </View>
+                :
+                <Button title = {'Take a picture'} icon = "camera" onPress = {takePicture}/> 
+            } 
             </View>
        </View>
       );
+    }
+
+    const takePicture = async () => {
+        if(cameraRef){
+            try{
+                const data = await cameraRef.current.takePictureAsync();
+                console.log(data);
+                setImage(data.url);
+            }
+            catch(e){
+                console.log(e);
+            }
+        }
     }
     
 
