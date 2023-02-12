14b6445cb97275de375bee04b6c69a590562d870
TayisiyaChernenko <tcx190026@utdallas.edu>
2023-02-12 04:23:42 -0600

    Requesting camera access

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { useCallback, useState } from 'react';
+import { useCallback, useState , useEffect, useRef} from 'react';
 import { Image, ImageBackground, Pressable, Text, View } from 'react-native';
+import * as MediaLibrary from "expo-media-library"
 import { useFonts } from 'expo-font';
 
 // You can import from local files
@@ -9,6 +10,7 @@ import image from '../assets/home_background.png';
 // import style sheet and color palette
 const styles = require('../config/style');
 import colors from '../config/colors';
+import { Camera } from 'expo-camera';
 
 export default function Home({ navigation }) {
   {
@@ -27,7 +29,20 @@ export default function Home({ navigation }) {
   if (!fontsLoaded) {
     return null;
   }
-
+  const [hasCameraPermission,setHasCameraPermissions] = useState(null);
+  const [image,setImage] = useState(null);
+  const [type,setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+  
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermissions(cameraStatus.status =='granted');
+      })();
+  },[])
+  
   return (
     <View style={styles.container}>
       <ImageBackground source={image} style={styles.background_image}>
